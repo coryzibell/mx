0798c04c881ef7ce087468461ce829f167fe414d
Geoff Baum <gabaum10@gmail.com>
2026-06-02 13:10:44 -0400

    Revert "f52bwbv322zjz0cc"
    This reverts commit 9d05becab42a0249bad06523fd8007d21908b481.

--- src/surreal_db/queries.rs
+++ src/surreal_db/queries.rs
@@ -1158,7 +1158,8 @@ impl SurrealDatabase {
                         count() AS cnt
                     FROM knowledge
                     WHERE created_at > time::now() - duration::from::days(56)
-                    GROUP BY week_bucket",
+                    GROUP BY week_bucket
+                    ORDER BY week_bucket",
                 )
                 .await
                 .context("Failed to query growth sparkline")
